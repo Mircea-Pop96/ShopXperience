e70b3a02736f272ab31e2c2e6c49c3afd9ba6fce
Mircea Pop <78227472+Mircea-Pop96@users.noreply.github.com>
2024-04-15 13:37:15 +0300

    Created scheleton and connected database

---
+++ backend/ecommerce/src/main/java/com/shopExperience/ecommerce/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.shopExperience.ecommerce.enums;
+
+public enum UserRole {
+    ADMIN,
+    CUSTOMER
+}
---
+++ backend/ecommerce/src/test/java/com/shopExperience/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopExperience.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
