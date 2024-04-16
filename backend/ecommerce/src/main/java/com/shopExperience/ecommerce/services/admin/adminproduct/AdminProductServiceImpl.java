d00e6836c4f24d063294364d2844b6699f1df0cd
Mircea Pop <78227472+Mircea-Pop96@users.noreply.github.com>
2024-04-16 19:10:20 +0300

    Created delete product and find products by name

--- backend/ecommerce/src/main/java/com/shopExperience/ecommerce/services/admin/adminproduct/AdminProductServiceImpl.java
+++ backend/ecommerce/src/main/java/com/shopExperience/ecommerce/services/admin/adminproduct/AdminProductServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -45,5 +46,15 @@ public class AdminProductServiceImpl implements AdminProductService {
         List<Product> products = productRepository.findAllByNameContaining(name);
 
         return products.stream().map(Product::getDto).collect(Collectors.toList());
+    }
+
+    public boolean deleteProduct(Long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
+        if(optionalProduct.isPresent()){
+            productRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
--- frontend/ShopExperience/src/app/admin/components/dashboard/dashboard.component.ts
+++ frontend/ShopExperience/src/app/admin/components/dashboard/dashboard.component.ts
@@ -5,21 +5,56 @@ import { MatDividerModule } from '@angular/material/divider';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import {
+  MatFormField,
+  MatFormFieldModule,
+  MatLabel,
+} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatDividerModule, RouterModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatDividerModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   products: any[] = [];
+  searchProductForm!: FormGroup;
 
-  constructor(private adminService: AdminService) {}
+  constructor(
+    private adminService: AdminService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
     this.getAllProducts();
+    this.searchProductForm = this.fb.group({
+      title: [null, [Validators.required]],
+    });
   }
 
   getAllProducts() {
@@ -29,6 +64,34 @@ export class DashboardComponent {
         element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
         this.products.push(element);
       });
+    });
+  }
+
+  submitForm() {
+    this.products = [];
+    const title = this.searchProductForm.get('title').value;
+    this.adminService.getAllProductsByName(title).subscribe((res) => {
+      res.forEach((element) => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+      console.log(this.products);
+    });
+  }
+
+  deleteProduct(productId: any) {
+    this.adminService.deleteProduct(productId).subscribe((res) => {
+      if (!res) {
+        this.snackBar.open('Product Deleted Succesfully!', 'Close', {
+          duration: 5000,
+        });
+        this.getAllProducts();
+      } else {
+        this.snackBar.open(res.message, 'Close', {
+          duration: 5000,
+          panelClass: 'error-snackbar',
+        });
+      }
     });
   }
 }
